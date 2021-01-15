a611a9e9fcf01d9e90d2a83e3787f889d420831f
rvallet <remy.vallet@gmail.com>
2021-01-15 14:25:24 +0100

    replace Mockito with PowerMock

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/cache/CompteComptableDaoCacheTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/cache/CompteComptableDaoCacheTest.java
@@ -1,20 +1,27 @@
 package com.dummy.myerp.consumer.dao.impl.cache;
 
+import com.dummy.myerp.consumer.ConsumerHelper;
 import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
 import com.dummy.myerp.consumer.dao.contrat.DaoProxy;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
 
 
 
-@ExtendWith(MockitoExtension.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConsumerHelper.class})
 public class CompteComptableDaoCacheTest {
 
     @Mock
@@ -26,14 +33,17 @@ public class CompteComptableDaoCacheTest {
     @InjectMocks
     CompteComptableDaoCache compteComptableDaoCache;
 
-    @Before
+/*    @Before
     public void initialize(){
         MockitoAnnotations.initMocks(this);
-    }
+    }*/
 
     @Test
     public void testgetByNumero(){
-    when(daoProxy.getComptabiliteDao()).thenReturn(daoProxy);
+    mockStatic(ConsumerHelper.class);
+    when(ConsumerHelper.getDaoProxy()).thenReturn(daoProxy);
+    when(daoProxy.getComptabiliteDao()).thenReturn(comptabiliteDao);
+    
     }
 
 
