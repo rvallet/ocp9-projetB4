baa3b5417644d50bc42359415e60976aad0eaae7
rvallet <remy.vallet@gmail.com>
2021-01-15 15:49:23 +0100

    Test Compte et Journal Cache (Mock)

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/cache/CompteComptableDaoCacheTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/cache/CompteComptableDaoCacheTest.java
@@ -3,14 +3,11 @@ package com.dummy.myerp.consumer.dao.impl.cache;
 import com.dummy.myerp.consumer.ConsumerHelper;
 import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
 import com.dummy.myerp.consumer.dao.contrat.DaoProxy;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -33,17 +30,26 @@ public class CompteComptableDaoCacheTest {
     @InjectMocks
     CompteComptableDaoCache compteComptableDaoCache;
 
-/*    @Before
-    public void initialize(){
-        MockitoAnnotations.initMocks(this);
-    }*/
-
     @Test
-    public void testgetByNumero(){
-    mockStatic(ConsumerHelper.class);
-    when(ConsumerHelper.getDaoProxy()).thenReturn(daoProxy);
-    when(daoProxy.getComptabiliteDao()).thenReturn(comptabiliteDao);
-    
+    public void testCompteComptableDaoCacheGetByNumero(){
+        PowerMockito.mockStatic(ConsumerHelper.class);
+        when(ConsumerHelper.getDaoProxy()).thenReturn(daoProxy);
+        when(daoProxy.getComptabiliteDao()).thenReturn(comptabiliteDao);
+
+        PowerMockito.spy(compteComptableDaoCache);
+        int i = 0;
+        int nbAppel = 5;
+        while(i<nbAppel) {
+            compteComptableDaoCache.getByNumero(i);
+            i+=1;
+            }
+
+        verify(comptabiliteDao, Mockito.times(1)).getListCompteComptable();
+        assertEquals(
+                "Le nombre d'appel au cache du CompteComptable ne correspond pas \n",
+                5,
+                i
+        );
     }
 
 
