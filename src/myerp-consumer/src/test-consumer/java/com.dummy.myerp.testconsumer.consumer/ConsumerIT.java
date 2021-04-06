eec6f6b82754a525caeab901e0a3b4b18bcb7a35
rvallet <remy.vallet@gmail.com>
2021-04-06 15:35:43 +0200

    init travis test

--- src/myerp-consumer/src/test-consumer/java/com.dummy.myerp.testconsumer.consumer/ConsumerIT.java
+++ src/myerp-consumer/src/test-consumer/java/com.dummy.myerp.testconsumer.consumer/ConsumerIT.java
@@ -1,12 +1,20 @@
 package com.dummy.myerp.testconsumer.consumer;
 
 import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
+import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration("classpath:bootstrapContext.xml")
@@ -21,8 +29,17 @@ public class ConsumerIT {
     }
 
     @Test
+    @Sql(executionPhase = BEFORE_TEST_METHOD, scripts = "classpath:/sql/clean_dist_demo_data.sql")
+    @Sql(executionPhase = BEFORE_TEST_METHOD, scripts = "classpath:/sql/init_dist_demo_data.sql")
+    @Sql(executionPhase = AFTER_TEST_METHOD, scripts = "classpath:/sql/clean_dist_demo_data.sql")
     public void getListCompteComptable(){
+        List<CompteComptable> compteComptableList = comptabiliteDao.getListCompteComptable();
 
+        Assert.assertEquals(
+                "Liste des comptes comptables",
+                7,
+                compteComptableList.size()
+        );
     }
 
     @Test
