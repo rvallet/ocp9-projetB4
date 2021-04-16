b12aadbd974cc871668c2e158c98b4635c9e95e2
rvallet <remy.vallet@gmail.com>
2021-04-16 09:51:07 +0200

    Inactive Docker and TI

--- docker/dev/init/db/docker-entrypoint-initdb.d/02_create_tables.sql
+++ docker/dev/init/db/docker-entrypoint-initdb.d/02_create_tables.sql
@@ -1,12 +1,12 @@
 
-CREATE TABLE public.journal_comptable (
+CREATE TABLE "remy-vallet_db_myerp".public.journal_comptable (
                 code VARCHAR(5) NOT NULL,
                 libelle VARCHAR(150) NOT NULL,
                 CONSTRAINT journal_comptable_pk PRIMARY KEY (code)
 );
 
 
-CREATE TABLE public.sequence_ecriture_comptable (
+CREATE TABLE "remy-vallet_db_myerp".public.sequence_ecriture_comptable (
                 journal_code VARCHAR(5) NOT NULL,
                 annee INTEGER NOT NULL,
                 derniere_valeur INTEGER NOT NULL,
@@ -14,9 +14,9 @@ CREATE TABLE public.sequence_ecriture_comptable (
 );
 
 
-CREATE SEQUENCE public.ecriture_comptable_id_seq;
+CREATE SEQUENCE "remy-vallet_db_myerp".public.ecriture_comptable_id_seq;
 
-CREATE TABLE public.ecriture_comptable (
+CREATE TABLE "remy-vallet_db_myerp".public.ecriture_comptable (
                 id INTEGER NOT NULL DEFAULT nextval('public.ecriture_comptable_id_seq'),
                 journal_code VARCHAR(5) NOT NULL,
                 reference VARCHAR(30),
@@ -26,16 +26,16 @@ CREATE TABLE public.ecriture_comptable (
 );
 
 
-ALTER SEQUENCE public.ecriture_comptable_id_seq OWNED BY public.ecriture_comptable.id;
+ALTER SEQUENCE "remy-vallet_db_myerp".public.ecriture_comptable_id_seq OWNED BY public.ecriture_comptable.id;
 
-CREATE TABLE public.compte_comptable (
+CREATE TABLE "remy-vallet_db_myerp".public.compte_comptable (
                 numero INTEGER NOT NULL,
                 libelle VARCHAR(150) NOT NULL,
                 CONSTRAINT compte_comptable_pk PRIMARY KEY (numero)
 );
 
 
-CREATE TABLE public.ligne_ecriture_comptable (
+CREATE TABLE "remy-vallet_db_myerp".public.ligne_ecriture_comptable (
                 ecriture_id INTEGER NOT NULL,
                 ligne_id INTEGER NOT NULL,
                 compte_comptable_numero INTEGER NOT NULL,
@@ -46,30 +46,30 @@ CREATE TABLE public.ligne_ecriture_comptable (
 );
 
 
-ALTER TABLE public.sequence_ecriture_comptable ADD CONSTRAINT journal_comptable_sequence_ecriture_comptable_fk
+ALTER TABLE "remy-vallet_db_myerp".public.sequence_ecriture_comptable ADD CONSTRAINT journal_comptable_sequence_ecriture_comptable_fk
 FOREIGN KEY (journal_code)
-REFERENCES public.journal_comptable (code)
+REFERENCES "remy-vallet_db_myerp".public.journal_comptable (code)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.ecriture_comptable ADD CONSTRAINT journal_comptable_ecriture_comptable_fk
+ALTER TABLE "remy-vallet_db_myerp".public.ecriture_comptable ADD CONSTRAINT journal_comptable_ecriture_comptable_fk
 FOREIGN KEY (journal_code)
-REFERENCES public.journal_comptable (code)
+REFERENCES "remy-vallet_db_myerp".public.journal_comptable (code)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.ligne_ecriture_comptable ADD CONSTRAINT ecriture_comptable_ligne_ecriture_comptable_fk
+ALTER TABLE "remy-vallet_db_myerp".public.ligne_ecriture_comptable ADD CONSTRAINT ecriture_comptable_ligne_ecriture_comptable_fk
 FOREIGN KEY (ecriture_id)
-REFERENCES public.ecriture_comptable (id)
+REFERENCES "remy-vallet_db_myerp".public.ecriture_comptable (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.ligne_ecriture_comptable ADD CONSTRAINT compte_comptable_ligne_ecriture_comptable_fk
+ALTER TABLE "remy-vallet_db_myerp".public.ligne_ecriture_comptable ADD CONSTRAINT compte_comptable_ligne_ecriture_comptable_fk
 FOREIGN KEY (compte_comptable_numero)
-REFERENCES public.compte_comptable (numero)
+REFERENCES "remy-vallet_db_myerp".public.compte_comptable (numero)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
